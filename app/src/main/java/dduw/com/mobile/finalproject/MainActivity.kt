9a5ee391d711b021a2b34a2a7c93b84be2b35317
seoyoungbyun <qustjdud2181@gmail.com>
2024-11-27 15:10:36 +0900

    Initial commit

---
+++ app/src/main/java/dduw/com/mobile/finalproject/MainActivity.kt
@@ -0,0 +1,21 @@
+package dduw.com.mobile.finalproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
